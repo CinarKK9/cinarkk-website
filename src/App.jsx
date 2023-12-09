c8cf8acbfa24227e7c380d636969569106f7c284
CinarKK9 <98798154+CinarKK9@users.noreply.github.com>
2023-12-09 11:57:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
       <h1>CinarKK</h1>
       <p>Useless website...</p>
       <p>please give me ideas</p>
-     <h2>NOTE: This site is NOT an IP grabber. It's against the AWS ToS.</h2>
+     <p><b>NOTE: This site is NOT an IP grabber. It's against the AWS ToS.</b></p>
       <a href="/add-ideas"><button>Add ideas here</button></a>
     </main>
     </>
