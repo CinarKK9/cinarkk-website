48e134afd0c5143007e7c28aec9c338c7790b742
CinarKK9 <98798154+CinarKK9@users.noreply.github.com>
2023-12-09 11:13:43 +0300

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
                 <nav>
                     <a href="/">CinarKK</a>
                     <ul>
-                        <li><a href="/">Home</a></li>
+                        
                         <li><a href="/ideas">Ideas</a></li>
                         <li><a href="/live-chat">Live Chat</a></li>
                     </ul>
