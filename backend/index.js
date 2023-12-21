704ea28a6c286e5e923a021883f5e457d99f3a93
CinarKK9 <98798154+CinarKK9@users.noreply.github.com>
2023-12-21 22:19:54 +0300

    added api to bite

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const app = require('express')()
+const axios = require('axios')
+
+app.get('/api/biteKey', (req, res) => {
+    const mail = req.query.email
+    const UID = req.query.uid
+    
+    if (!UID && !mail) {
+        return res.status(400).send('enter uid or email');
+    }
+    axios.get(`https://biteyt.xyz/api?searchKeyBy${UID ? `UID=${UID}` : `EMail=${mail}`}`).then((response) => {
+        res.send(response.data)
+    }).catch((err) => {
+        res.status(500).send(err)
+    });
+})
+
+app.listen(3000, () => {
+    console.log('API IS WORKING IN PORT 3000')
+})
