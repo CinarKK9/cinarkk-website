c4898a80ac1948b6ba7674e728753de9ec632ecb
CinarKK9 <98798154+CinarKK9@users.noreply.github.com>
2023-12-21 22:25:05 +0300

    nice

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.get('/api/biteKey', (req, res) => {
     if (!UID && !mail) {
         return res.status(400).send('enter uid or email');
     }
-    axios.get(`https://biteyt.xyz/api?searchKeyBy${UID ? `UID=${UID}` : `EMail=${mail}`}`).then((response) => {
+    axios.get(`https://biteyt.xyz/api?searchKeyBy${UID ? `UID=${UID}` : `Email=${mail}`}`).then((response) => {
         res.send(response.data)
     }).catch((err) => {
         res.status(500).send(err)
